dbc1dda02150a360b5fbaf50d039471c6e864cbf
mihaosin <mikhail.osinovskiy@mail.ru>
2022-08-31 22:30:10 +0300

    создано новый проект MantisBT_2.25.4 начато тестирование функций login, logout остановились на поиске имени пользователя на стартовой странице

---
+++ fixture/session.py
@@ -0,0 +1,53 @@
+from selenium.webdriver.common.by import By
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        # этот конструктор получает в качестве внешнего параметра ссылку на объект класса Application
+        # и сохраняет ее в свойство с именем app
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        # авторизация в учетной записи
+        wd.find_element(by=By.NAME, value="username").click()
+        wd.find_element(by=By.NAME, value="username").clear()
+        wd.find_element(by=By.NAME, value="username").send_keys(username)
+        wd.find_element(by=By.CSS_SELECTOR, value="input[value='Вход']").click()
+        wd.find_element(by=By.NAME, value="password").click()
+        wd.find_element(by=By.NAME, value="password").clear()
+        wd.find_element(by=By.NAME, value="password").send_keys(password)
+        wd.find_element(by=By.CSS_SELECTOR, value="input[value='Вход']").click()
+
+    def logout(self):
+        wd = self.app.wd
+        # выход из учетной записи
+        wd.find_element(by=By.LINK_TEXT, value="Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_loggeg_in_user() == username
+
+    def get_loggeg_in_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text[1:-1]
+
+    def ensure_login(self, username, password):
+
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username,password)
